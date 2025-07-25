83bee4e47541e945bd8fdc41b16d4a2f9b9964b7
Pitiwat <ppxpl2@nottingham.ac.uk>
2025-07-25 16:20:39 +0100

    fix:config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ADD THIS LINE FOR GITHUB PAGES DEPLOYMENT
+  // Replace 'your-repository-name' with the actual name of your GitHub repository.
+  // For example, if your repository is named 'zensenstock-front', it would be '/zensenstock-front/'
+  base: '/zensenstock-front/', 
 })
