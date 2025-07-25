f583a041eaeb18ff46ab4770f3390c8cf6907f11
Pitiwat <ppxpl2@nottingham.ac.uk>
2025-07-25 17:08:59 +0100

    fix:config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/zensestock-front/', // 👈 use your repo name here
   plugins: [react()],
-  // **THIS LINE IS CRITICAL:**
-  base: '/zensestock-front/', // MAKE SURE IT'S EXACTLY THIS
-})
+});
 
